a52e0b5d47eb1ac4a266411e3583cac5552aa125
Sarma Tangirala <tvssarma.omega9@gmail.com>
2015-07-18 17:40:02 -0700

    WIP fixing stuff.

--- src/pygitadd.py
+++ src/pygitadd.py
@@ -1,9 +1,6 @@
 import sys
 import errn
-
-def compute_string_hash(unhashed_string):
-  # Simply return the same string for now.
-  return unhashed_string
+import utils
 
 def pygit_add(filename):
   ''' Main method to add files to the current index. '''
@@ -12,19 +9,13 @@ def pygit_add(filename):
   # TODO Figure out how a git repo is identified. I assume it's recursive search. This allows
   # for submodules. Test if git repo would fail here for such a search.
   try:
-  index_dict = read_object_from_file.read_object_from_file('.pygit/index_file')
-  temp_index_dict = read_object_from_file.read_object_from_file('.pygit/temp_index_file')
-  except OSError as e:
-    if e.errno == errno.ENOENT:
-      print 'Failed to read index. Broke repo.'
-      sys.exit(-1)
-  if compute_string_hash(filename) in index_dict:
-    # compute diff and do other magic.
-  else:
-    temp_index_dict.add
-
-
-
-
-
-
+    index_dict = utils.read_object_from_file('.pygit/index_file')
+    temp_index_dict = utils.read_object_from_file('.pygit/temp_index_file')
+    except OSError as e:
+      if e.errno == errno.ENOENT:
+        print 'Failed to read index. Broke repo.'
+        sys.exit(-1)
+    if utils.compute_string_hash(filename) in index_dict:
+      # compute diff and do other magic.
+    else:
+      temp_index_dict.add
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,18 @@
+import sys
+import zlib
+import base64
+import cPickle
+
 def read_object_from_file(file_name):
   try:
-    with open(file_name, 'r') as f:
-      decoded_file_string = base64.b64decode(file_name)
+    with open(file_name, 'r') as fp:
+      decoded_file_string = base64.b64decode(fp)
       uncompressed_pickled_object = zlib.decompress(decoded_file_string)
-      unpickled_object = cPickle.loads(uncompressed_file_string)
+      unpickled_object = cPickle.loads(uncompressed_pickled_object)
 
       return unpickled_object
   except OSError:
-    print 'Failed to read object from file -- ', read_file
+    sys.stderr.write('Failed to read object from file -- ' + file_name + '\n')
     raise
 
 def write_object_to_file(file_name, obj):
@@ -15,8 +20,12 @@ def write_object_to_file(file_name, obj):
   compressed_object_string = zlib.compress(pickled_object, 9)
   encoded_file_string = base64.encodestring(compressed_object_string)
   try:
-    with open(out_file, 'w') as f:
-      f.write(encoded_file_string)
-  except OSError as e:
-    print 'Failed to write object to file.'
+    with open(file_name, 'w') as fp:
+      fp.write(encoded_file_string)
+  except OSError:
+    sys.stderr.write('Failed to write object to file -- ' + file_name + '\n')
     raise
+
+def compute_string_hash(unhashed_string):
+  ''' Simply return the same string for now. '''
+  return unhashed_string
